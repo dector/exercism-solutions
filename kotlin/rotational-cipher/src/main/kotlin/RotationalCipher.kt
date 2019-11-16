8ab09810cfd49092ad67f0e58656b708cb8d26d4
Denys M <dector9@gmail.com>
2019-11-16 19:07:53 +0400

    Kotlin. RotationalCipher. Solution.

--- kotlin/rotational-cipher/src/main/kotlin/RotationalCipher.kt
+++ kotlin/rotational-cipher/src/main/kotlin/RotationalCipher.kt
@@ -1,8 +1,23 @@
-class RotationalCipher {
+class RotationalCipher(private val shift: Int) {
 
-    // TODO: Implement proper constructor
+    fun encode(text: String): String = text
+        .map(this::encode)
+        .joinToString("")
 
-    fun encode(text: String): String {
-        TODO("Implement this function to complete the task")
+    private fun encode(char: Char): Char {
+        val normalized = if (char.isUpperCase()) char.toLowerCase() else char
+
+        if (!RAW_ALPHABET.contains(normalized)) return char
+
+        val encoded = run {
+            val index = RAW_ALPHABET.indexOf(normalized) + shift
+            RAW_ALPHABET[index % 26]
+        }
+
+        return if (char.isUpperCase()) encoded.toUpperCase() else encoded
+    }
+
+    private companion object {
+        const val RAW_ALPHABET = "abcdefghijklmnopqrstuvwxyz"
     }
 }
