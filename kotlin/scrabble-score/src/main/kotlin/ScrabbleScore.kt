f044393be58dc0c1b695e7fae8601a629e2b0bd3
Denys M <dector9@gmail.com>
2019-11-18 18:14:40 +0400

    Kotlin. Scrabble Score. Solution.

--- kotlin/scrabble-score/src/main/kotlin/ScrabbleScore.kt
+++ kotlin/scrabble-score/src/main/kotlin/ScrabbleScore.kt
@@ -1,10 +1,21 @@
 object ScrabbleScore {
 
-    fun scoreLetter(c: Char): Int {
-        TODO("Implement this function to complete the task")
-    }
+    private val score = mapOf(
+        "aeioulnrst" to 1,
+        "dg" to 2,
+        "bcmp" to 3,
+        "fhvwy" to 4,
+        "k" to 5,
+        "jx" to 8,
+        "qz" to 10
+    ).flatMap { (letters, score) ->
+        letters.map { it to score }
+    }.toMap()
 
-    fun scoreWord(word: String): Int {
-        TODO("Implement this function to complete the task")
-    }
+    fun scoreLetter(c: Char): Int =
+        score.getValue(c.toLowerCase())
+
+    fun scoreWord(word: String): Int =
+        word.map(this::scoreLetter)
+            .sum()
 }
